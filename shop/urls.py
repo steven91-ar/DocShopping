ee5cbf424f4b4b9cf966f1ebb5cff01265062fdc
steven9work <72232262+steven91-ar@users.noreply.github.com>
2025-03-11 15:37:31 +0100

    auth

--- shop/urls.py
+++ shop/urls.py
@@ -1,13 +1,16 @@
+from itertools import product
+
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from django.contrib import admin
 from django.urls import path
-from store.views import index
+from store.views import index, product_detail
 
 from shop import settings
 
 urlpatterns = [
     path('', index, name='index'),
     path('admin/', admin.site.urls),
+    path('product/<str:slug>/', product_detail, name='product'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- store/models.py
+++ store/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 """
@@ -12,7 +13,8 @@ Product
 """
 
 class Product(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=128)
+    slug = models.SlugField(max_length=128)
     price = models.FloatField(default=0.0)
     stock = models.IntegerField(default=0)
     description = models.TextField(blank=True)
@@ -20,3 +22,6 @@ class Product(models.Model):
 
 def __str__(self):
     return self.name
+
+def get_absolute_url(self):
+    return reverse("product", kwargs={"slug": self.slug})
