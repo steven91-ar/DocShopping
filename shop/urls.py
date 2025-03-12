f04bf8684e6b1fdcfaa437846832bd4092e74cc4
steven9work <72232262+steven91-ar@users.noreply.github.com>
2025-03-12 12:39:47 +0100

    gestion utilisateur

--- shop/urls.py
+++ shop/urls.py
@@ -5,6 +5,8 @@ from django.contrib import admin
 from django.urls import path
 from django.contrib import admin
 from django.urls import path
+
+from accounts.views import signup
 from store.views import index, product_detail
 
 from shop import settings
@@ -12,5 +14,6 @@ from shop import settings
 urlpatterns = [
     path('', index, name='index'),
     path('admin/', admin.site.urls),
+    path('signup/', signup, name="signup"),
     path('product/<str:slug>/', product_detail, name='product'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
